7c9318d22ea04b9f8048b9d68054b72c9fc0570e
derrick b <dbozich1@gmail.com>
2018-06-01 04:41:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ class Song
 
      artist, song = filename.split(" - ")
      new_song = self.new(song)
-     new_song.artist_name = Artist.find_or_create_by_name(artist)
+     new_song.artist_name = artist
      new_song
 
   end
